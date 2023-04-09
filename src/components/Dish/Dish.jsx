5cf95a08a5784a8454d18bd82a1674d84ae80800
droganov <droganov@gontardcie.com>
2023-04-10 02:13:02 +0600

    Dish-ready

--- src/components/Dish/Dish.jsx
+++ src/components/Dish/Dish.jsx
@@ -1,27 +1,22 @@
-import { Button } from "@/components/Button/Button";
 import React from "react";
 
-import ThumbDown from "./images/thumb-down.svg";
-
-import styles from "./styles.module.scss";
-
 export const Dish = ({ dish }) => {
-  if (!dish) {
-    return null;
-  }
-
-  const { name, ingredients } = dish;
+  if (!dish ) return null;
 
   return (
     <div>
-      <span>{name}</span>
-      <Button className={styles.decrementAction} type="secondary">
-        -
-      </Button>
-      0
-      <Button className={styles.incrementAction} type="primary">
-        +
-      </Button>
+      <div>
+        <span>название: </span>
+        <span>{dish.name}</span>
+      </div>
+      <div>
+        <span>цена: </span>
+        <span>{dish.price}</span>
+      </div>
+      <div>
+        <span>состав: </span>
+        <span>{dish.ingredients.join(', ')}</span>
+      </div>
     </div>
   );
 };
