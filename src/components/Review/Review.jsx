60bb1e03b04569bc02e479fc6490155d16355b9c
droganov <droganov@gontardcie.com>
2023-04-13 00:33:58 +0600

    images

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import Image from 'next/image.js'
 
 export const Review = ({ review }) => {
-  if (!review ) return null;
+  if (!review) {
+    return null;
+  }
 
-  const { user, text, rating } = review
-
+  const { user, text, rating } = review;
+  const maxRating = 5;
+  const ratingImgs = []
+  for(let i = 0; i<maxRating; i++) {
+    ratingImgs.push(i<rating?'star-gold':'star')
+  }
   return (
     <div>
-      <h5>{user}</h5>
+      <p>{user}</p>
+      {<p>{ratingImgs.map((ratingImg,index)=>(<Image  key={index} src={`/${ratingImg}.png`}  alt="no rating image"/>))}</p>}
       <p>{text}</p>
-      <p>{rating}</p>
     </div>
   );
 };
