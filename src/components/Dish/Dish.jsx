0de365dc1fc07bb31584924aacc54daced8c5d34
droganov <droganov@gontardcie.com>
2023-04-20 20:09:51 +0600

    amount plus/minus

--- src/components/Dish/Dish.jsx
+++ src/components/Dish/Dish.jsx
@@ -1,25 +1,34 @@
 import { Button } from "@/components/Button/Button";
-import React from "react";
-
-import ThumbDown from "./images/thumb-down.svg";
+import React, {useState} from 'react'
 
 import styles from "./styles.module.scss";
 
 export const Dish = ({ dish }) => {
+  const AMOUNT_INI = 0
+  const [amount,setAmount] = useState(AMOUNT_INI)
   if (!dish) {
     return null;
   }
 
-  const { name, ingredients } = dish;
+  const { name } = dish;
 
   return (
     <div>
       <span>{name}</span>
-      <Button className={styles.decrementAction} type="secondary">
+      <Button
+        className={styles.decrementAction}
+        type="secondary"
+        onClick={()=>setAmount(amount - 1)}
+        disabled={amount===0}>
         -
       </Button>
-      0
-      <Button className={styles.incrementAction} type="primary">
+      {amount}
+      <Button
+        className={styles.incrementAction}
+        type="primary"
+        onClick={()=>setAmount(amount + 1)}
+        disabled={amount===5}
+      >
         +
       </Button>
     </div>
